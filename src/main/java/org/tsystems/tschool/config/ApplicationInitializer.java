977c9e17319a63e7f75f5aaa50f9e3f8bc9ab0f2
sergey <sergei.ni@yandex.ru>
2020-09-03 20:29:54 +0300

    DispatcherServlet configuration

---
+++ src/main/java/org/tsystems/tschool/config/ApplicationInitializer.java
@@ -0,0 +1,22 @@
+package org.tsystems.tschool.config;
+
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class ApplicationInitializer extends
+        AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebMvcConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
