a3a367e202eeeb16cb1822852509dfa668f593c5
sergey <sergei.ni@yandex.ru>
2020-10-18 01:49:53 +0300

    updated logs

---
+++ market-v2/src/test/java/org/tsystems/tschool/service/ArticleServiceTest.java
@@ -0,0 +1,53 @@
+package org.tsystems.tschool.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArticleServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void removeArticleById() {
+    }
+
+    @Test
+    void saveArticle() {
+    }
+
+    @Test
+    void updateArticle() {
+    }
+
+    @Test
+    void getAllCategoriesAndValuesByArticleId() {
+    }
+
+    @Test
+    void addValue() {
+    }
+
+    @Test
+    void deleteValue() {
+    }
+
+    @Test
+    void getCatalog() {
+    }
+
+    @Test
+    void getArticlesRating() {
+    }
+}
