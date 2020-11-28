2ec64f1d3c5f4ce7cd9a77b6b5ababe372a5f7dd
sereggan <sergei.ni@yandex.ru>
2020-11-28 09:48:08 +0300

    moved to spring boot

--- market-v2/src/main/java/org/tsystems/tschool/exception/ItemNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package org.tsystems.tschool.exception;
-
-public class ItemNotFoundException extends RuntimeException {
-
-    public ItemNotFoundException(String message) {
-        super(message);
-    }
-}
